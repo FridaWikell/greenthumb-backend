76e0bb7ae13d2a6b39e9cc9cf88efa9464287e0e
Frida Wikell <fridasvensson7@gmail.com>
2024-04-16 15:07:58 +0000

    Add: Corsheaders and allowed hosts in settings

--- greenthumb/settings.py
+++ greenthumb/settings.py
@@ -35,9 +35,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-fridawikell-greenthumbb-mqfsmpi9xz9.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-fridawikell-greenthumbb-mqfsmpi9xz9.ws-eu110.gitpod.io', 'greenthumb-back-4bd145d8f205.herokuapp.com']
 
 
 # Application definition
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'allauth.account', 
     'allauth.socialaccount', 
     'dj_rest_auth.registration',
+    'corsheaders',
     'profiles',
     'posts',
     'comments',
@@ -90,10 +91,12 @@ REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'greenthumb.serializers.CurrentUserSerializer'}
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -102,6 +105,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'greenthumb.urls'
 
