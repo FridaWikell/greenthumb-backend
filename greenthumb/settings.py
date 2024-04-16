6bbcfd991018e0bfb0e0f3d533f45161d006ce53
Frida Wikell <fridasvensson7@gmail.com>
2024-04-16 12:42:05 +0000

    Add: Create profiles app, add model and add in allowed hosts

--- greenthumb/settings.py
+++ greenthumb/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-+fm0hw+zugyr_im)2q*4288$v(6uq+4-%hcuwc=nh!imyltua^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-fridawikell-greenthumbb-mqfsmpi9xz9.ws-eu110.gitpod.io']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'profiles',
 ]
 
 MIDDLEWARE = [
