bc4ccbba7b30f3413f01915c95a571b8b423b88a
Frida Wikell <fridasvensson7@gmail.com>
2024-04-17 07:26:58 +0000

    Add: Access and refresh tokens and fix typo in axiosReq

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -3,5 +3,5 @@ import axios from "axios";
 axios.defaults.headers.post["content-type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-export const axiosRec = axios.create();
+export const axiosReq = axios.create();
 export const axiosRes = axios.create();
