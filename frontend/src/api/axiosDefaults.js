60865caa80a5d30c130b7da302b97cf5bd1438ee
Frida Wikell <fridasvensson7@gmail.com>
2024-04-17 06:27:08 +0000

    Add: Axios defaults file and update package json with proxy

---
+++ frontend/src/api/axiosDefaults.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+axios.defaults.headers.post["content-type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosRec = axios.create();
+export const axiosRes = axios.create();
