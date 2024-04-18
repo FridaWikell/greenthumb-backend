7b4834e88324e9952979e92f3d70fdd5c6f27ed8
Frida Wikell <fridasvensson7@gmail.com>
2024-04-18 11:57:35 +0000

    Add: Change question serializer to create answers at the same time

--- polls/serializers.py
+++ polls/serializers.py
@@ -2,19 +2,26 @@ from rest_framework import serializers
 from .models import Question, Answer, Vote
 
 
-class AnswerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Answer
-        fields = ['id', 'text', 'question', 'created_at']
-
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
         fields = ['id', 'answer', 'voter', 'created_at']
 
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ['id', 'text', 'created_at']
+
 class QuestionSerializer(serializers.ModelSerializer):
-    answers = AnswerSerializer(many=True, read_only=True)
+    answers = AnswerSerializer(many=True, read_only=False, required=False)
 
     class Meta:
         model = Question
         fields = ['id', 'owner', 'text', 'created_at', 'answers']
+
+    def create(self, validated_data):
+        answers_data = validated_data.pop('answers', [])
+        question = Question.objects.create(**validated_data)
+        for answer_data in answers_data:
+            Answer.objects.create(question=question, **answer_data)
+        return question
