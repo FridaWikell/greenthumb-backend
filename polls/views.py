02ffcc2881d0682994c5eed3ced7a1353d0b5038
Frida Wikell <fridasvensson7@gmail.com>
2024-04-18 10:09:26 +0000

    Add: Urls in both polls and main urls and add views in polls

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Question, Answer, Vote
+from .serializers import QuestionSerializer, AnswerSerializer, VoteSerializer
 
-# Create your views here.
+
+# Questions
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+# Answers
+class AnswerList(generics.ListCreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+class AnswerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+# Votes
+class VoteList(generics.ListCreateAPIView):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
+class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
